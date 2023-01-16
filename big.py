105de46342806f6c8e10d6bc99fa654b8c646e02
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-17 01:49:07 +0300

    Update big.py

--- big.py
+++ big.py
@@ -1,5 +1,4 @@
-class sum_of_array():
-    given_array = [1,2,3,4,5,6,7,8,9,10]
+given_array = [1,2,3,4,5,6,7,8,9,10]
 
     def find_sum(given_array):
         total = 0
@@ -7,5 +6,3 @@ class sum_of_array():
         for i in given_array:
             total += i
         return total
-
-    find_sum(given_array)
