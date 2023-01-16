86a409a3fcaa94582f286f28458731a13cf9f953
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-17 01:50:18 +0300

    Update big.py

--- big.py
+++ big.py
@@ -1,7 +1,7 @@
 given_array = [1,2,3,4,5,6,7,8,9,10]
 
-    def find_sum(given_array):
-        total = 0
+    def find_sum(given_array): 
+        total = 0  # O(n)
 
         for i in given_array:
             total += i
