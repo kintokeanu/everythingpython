13e57429bacbe44eae264d44520c0a09f673fd8a
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-17 01:48:34 +0300

    Create big.py

---
+++ big.py
@@ -0,0 +1,11 @@
+class sum_of_array():
+    given_array = [1,2,3,4,5,6,7,8,9,10]
+
+    def find_sum(given_array):
+        total = 0
+
+        for i in given_array:
+            total += i
+        return total
+
+    find_sum(given_array)
