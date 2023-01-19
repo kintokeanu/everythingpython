5d18bcfc8902ecbf1e8f621bc0c0fe3ac0e61fb1
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-19 22:41:47 +0300

    Create site.py
    site connectivity checker

---
+++ site.py
@@ -0,0 +1,15 @@
+import urllib.request as urllib
+
+while True:
+    def main(url):
+        print("checking conectivity")
+
+        response = urllib.urlopen(url)
+        print("connected to", url, "successfully")
+        print("the response code was: ", response.getcode())
+
+    print("Do you want to check if a sit is up? ")
+    print("")
+    input_url = input("enter site you want to check: ")
+
+    main(input_url)
