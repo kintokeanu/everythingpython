34cc5c93e6ca0e0cd5821c81a8d242bbba247822
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-20 03:38:56 +0300

    Update guess.py

--- guess.py
+++ guess.py
@@ -14,4 +14,26 @@ def guess(x):
     
     print(f"Yes!! you guessed {random_number} correctly")
 
-guess(10)
+
+    
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f" Is the {guess} to high(H), too low (L) or correct (c)").lower()
+        if feedback == "h":
+            high = guess -1
+        elif feedback == 'l':
+            low = guess+ 1
+    print(f"Yes!! you guessed the number {guess} correctly")
+
+
+computer_guess(10)
+
+    
+    
