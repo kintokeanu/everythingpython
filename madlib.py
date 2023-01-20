5bd754af58e7c672a74612c55bd8d8ab8311ae4d
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-20 03:02:04 +0300

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,11 @@
+noun1 = input("Enter noun: ")
+noun2 = input("Enter noun: ")
+noun3 = input("Enter noun: ")
+int1 = input("Enter score: ")
+int2 = input("Enter score: ")
+
+
+mad_libs = f"im brian {noun1} i like watching football. My favorite team is {noun2}. our biggest \
+rivals are {noun3}. we beat them by {int1} goals to {int2}"
+
+print(mad_libs)
