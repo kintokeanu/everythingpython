24683cf7bba5d993218afabaebe8e981fca4b9ac
Brian Levu <75661862+kintokeanu@users.noreply.github.com>
2023-01-24 21:40:44 +0300

    Create count.py
    this program allows a user to count the number of words in a given string

---
+++ count.py
@@ -0,0 +1,7 @@
+test_string = "We have a lot of tasks/problems every day that we have to solve to survive. Some of these tasks are simple to achieve while others are hard. We have to do the hard things to solve bigger problems. The hardest thing to achieve is consistency and with that, we’ll be able to solve hard problems. In the past ive lived to set standards that will assist me in achieving tougher goals efficiently"
+#original string
+print ("The original string is : " + test_string)
+# using split() function
+res = len(test_string.split())
+# total no of words
+print ("The number of words in string are : " + str(res))
